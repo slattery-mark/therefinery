62be4499ed479935338822674231a15c427388cc
slattery-mark <markys1@hotmail.com>
2022-05-24 18:32:54 -0400

    bug fixing

--- server/index.js
+++ server/index.js
@@ -7,12 +7,14 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Handle GET requests to /api route
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello from server!" });
 });
 
-// All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});   
