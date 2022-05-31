ae188430ab47b77d686b0fdd24e7ef4b1e965a9a
slattery-mark <markys1@hotmail.com>
2022-05-31 08:59:45 -0400

    npm run dev at top level launches server and client

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode>z
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
