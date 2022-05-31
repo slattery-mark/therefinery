0f0ab4eca4839dc3a771a85ddef5b477ce52cf5c
slattery-mark <markys1@hotmail.com>
2022-05-30 20:44:53 -0400

    added link to github and adjusted description font size

--- client/src/components/layout/footer/footer.js
+++ client/src/components/layout/footer/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <footer className='footer'>
             <p className='footer__description-text'>The Refinery is a game recommendation app that uses Steam’s API to suggest relevent games for any Steam user(s).</p>
-            <FontAwesomeIcon icon={faGithub} color='#FFFFFF' className='footer__github-logo' />
+            <a href='https://github.com/slattery-mark/therefinery' ><FontAwesomeIcon icon={faGithub} color='#FFFFFF' className='footer__github-logo' /></a>
         </footer>
     )
 }
