40bb1e68a3503da9d79655e2c200861bd4daff9b
slattery-mark <markys1@hotmail.com>
2022-06-01 11:05:13 -0400

    messing with hamburger styles

--- client/src/components/layout/sidebar.js
+++ client/src/components/layout/sidebar.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SearchBox from '../common/searchbox';
 import GreenButton from '../common/greenbutton';
 import './sidebar.css';
 
 const SideBar = () => {
-    const [open, setOpen] = useState(true);
-
     return (
         <div className='sidebar'>
             <SearchBox placeholder={"Enter Steam ID"} />
