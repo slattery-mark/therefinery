086169eaf9a9738a9088dd463f01f33a42f20b0d
slattery-mark <fe0846@wayne.edu>
2022-06-06 12:49:03 -0500

    code cleanup

--- client/src/components/layout/footer.js
+++ client/src/components/layout/footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import './footer.css';
 
 const Footer = () => {
