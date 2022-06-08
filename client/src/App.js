fc4173bccbec01c593791c981efe51b975994c3d
slattery-mark <fe0846@wayne.edu>
2022-06-08 14:37:50 -0500

    added results page routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/layout';
 import HomePage from "./components/views/homepage";
+import ResultsPage from './components/views/resultspage';
 import './App.css';
 
 function App() {
@@ -20,6 +21,10 @@ function App() {
     <Routes>
       <Route exact path='/' element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="/results/*">
+          <Route path=':firstID' element={<ResultsPage />} />
+          <Route path=':firstID+:secondID' element={<ResultsPage />} />
+        </Route>
       </Route>
     </Routes>
   );
--- client/src/components/views/resultspage.js
+++ client/src/components/views/resultspage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import './resultspage.css';
 
 const ResultsPage = () => {
+    const params = useParams();
+
+    React.useEffect(() => {
+        console.log(params);
+    }, []);
+
     return (
-        <>
-        </>
+        <div className='results-page'>
+        </div>
     )
 }
 
