d300e4ce90c8c4b1781eabcf29e8fc089b7c9f12
slattery-mark <fe0846@wayne.edu>
2022-06-08 14:15:54 -0500

    made searchbox more reuseable

--- client/src/components/common/searchbox.js
+++ client/src/components/common/searchbox.js
@@ -3,11 +3,17 @@ import './searchbox.css';
 
 /**
  * An input which functions as a search box.
- * @param {String} placeholder the placeholder text to appear when the input is empty 
+ * @param {String} placeholder  the placeholder text to appear when the input is empty 
+ * @param {String} name the name of the input (for labels)
+ * @param {String} value the initial value of the input
+ * @param {String} func the function to execute onChange
  */
-const SearchBox = ({ placeholder }) => {
+const SearchBox = ({ placeholder, value, name, func }) => {
     return (
-        <input type='text' className='searchbox' placeholder={ placeholder } />
+        <div className='searchbox'>
+            <label htmlFor={name} />
+            <input type='text' name={name} value={value} onChange={func} placeholder={placeholder} />
+        </div>
     )
 }
 
